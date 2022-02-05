296f46a362d61eae6ecf095f9ac41cdea6d6d341
Jamoliddin610 <najmiddinovjamoliddin571@gmail.com>
2022-02-05 16:24:20 +0500

    added hero section

---
+++ main.js
@@ -0,0 +1,28 @@
+let elMenuBox = document.getElementById('menubox');
+let elopenMenuBox = document.getElementById('openMenubox');
+let elcloseMenuBox = document.getElementById('closeMenubox');
+let ellogo = document.getElementById('logo');
+let ellogonight = document.getElementById('logoNight');
+let elmood =document.getElementById('night');
+let elzindex = document.getElementById('zIndex')
+
+elopenMenuBox.addEventListener('click',function(){
+    elMenuBox.classList.add('show');
+    ellogo.classList.add('hidelogo');
+    elcloseMenuBox.classList.add('modalX');
+    elzindex.classList.add('index')
+
+})
+elcloseMenuBox.addEventListener('click',function(){
+    elMenuBox.classList.remove('show');
+    ellogo.classList.remove('hidelogo');
+    elcloseMenuBox.classList.remove('modalX');
+    elzindex.classList.remove('index')
+
+})
+elmood.addEventListener('click', function(){
+    document.body.classList.toggle('dark');
+    ellogonight.classList.toggle('logofornight');
+    elopenMenuBox.classList.toggle('white');
+
+})
